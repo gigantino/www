a77f3b7174b30116d202d7f256aa82a9afbc7940
gigantino <git@gigantino.dev>
2023-09-20 23:59:12 +0200

    feat: add robots and sitemap, minor fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
+  site: "https://gigantino.dev",
   server: {
     port: 3000,
   },
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   output: "server",
   adapter: vercel(),
 });
