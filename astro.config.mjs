dd70796214286987b5d1322f3c553f86c210edfb
gigantino <git@gigantino.dev>
2023-10-05 01:51:45 +0200

    feat: switch to a static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 import svelte from "@astrojs/svelte";
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
   },
   integrations: [tailwind(), mdx(), sitemap(), partytown(), svelte()],
-  output: "server",
+  output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  const languageCode: string = navigator.language.split("-")[0];
+  const greetings = {
+    es: "hola",
+    fr: "bonjour",
+    de: "hallo",
+    it: "ciao",
+    pt: "olá",
+    nl: "hallo",
+    pl: "cześć",
+    sv: "hej",
+    da: "hej",
+    no: "hei",
+    fi: "hei",
+    el: "γειά σας",
+    hu: "helló",
+    cz: "ahoj",
+  };
+  const greeting = (greetings[languageCode as keyof typeof greetings] || "hello") as string;
+</script>
+
+<h1>{greeting}! 👋</h1>
