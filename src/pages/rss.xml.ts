d49d206f418318c2ea48ccfab15848fa64fb6eb3
gigantino <git@gigantino.dev>
2025-11-11 17:45:41 +0100

    chore: rename rss feed name, fix button font

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,7 +3,7 @@ import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async function get({ site }) {
   return rss({
-    title: "gg's blog",
+    title: "ggtn's blog",
     description: "In depth posts about technical challenges I've had the chance to delve into.",
     site: site ? site.toString() : "",
     items: await pagesGlobToRssItems(import.meta.glob("./blog/*.{md,mdx}")),
